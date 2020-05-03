b8f31781916bf23c57750876b21b17428a14b4fa
Ananthan <ananthakrishnan1011@gmail.com>
2020-05-03 21:14:48 +0530

    code refactoring and optimisations

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:expense_tracker/screens/homepage.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
